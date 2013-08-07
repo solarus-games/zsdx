48d525a39e1a921c5cbe468cb2cf4511f4449125
Christopho <christopho@solarus-games.org>
2013-08-07 12:15:53 +0200

    Remove usage of sol.language.get_default_language().

--- data/menus/language.lua
+++ data/menus/language.lua
@@ -19,7 +19,7 @@ function language_menu:on_started()
   else
 
     local ids = sol.language.get_languages()
-    local default_id = sol.language.get_default_language()
+    local default_id = "en"
     local index = 1
     local cursor_position = 1
     self.surface = sol.surface.create(320, 240)
