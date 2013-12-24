aa9eda9a594b05e0e3c5c4fd989402d9084cd954
Christopho <christopho@solarus-games.org>
2013-12-24 09:16:15 +0100

    Correctly update the hearts view when the max life is reduced. Fix #74.

--- data/hud/hearts.lua
+++ data/hud/hearts.lua
@@ -47,6 +47,12 @@ function hearts:check()
   local nb_max_hearts = self.game:get_max_life() / 4
   if nb_max_hearts ~= self.nb_max_hearts_displayed then
     need_rebuild = true
+
+    if nb_max_hearts < self.nb_max_hearts_displayed then
+      -- Decrease immediately if the max life is reduced.
+      self.nb_current_hearts_displayed = self.game:get_life()
+    end
+
     self.nb_max_hearts_displayed = nb_max_hearts
   end
 
@@ -113,6 +119,8 @@ function hearts:repeat_danger_sound()
 end
 
 function hearts:rebuild_surface()
+
+  self.surface:clear()
 
   -- Display the hearts.
   for i = 0, self.nb_max_hearts_displayed - 1 do
