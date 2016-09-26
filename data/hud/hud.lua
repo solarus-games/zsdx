f6a48ac633b9e5be8484d6a25ba227db74fc9ce9
Christopho <christopho@solarus-games.org>
2016-09-26 14:47:56 +0200

    Fix menus after require()

--- data/hud/hud.lua
+++ data/hud/hud.lua
@@ -1,201 +1,202 @@
-local game = ...
+return function(game)
 
-function game:initialize_hud()
+  function game:initialize_hud()
 
-  -- Set up the HUD.
-  local hearts_builder = require("hud/hearts")
-  local magic_bar_builder = require("hud/magic_bar")
-  local rupees_builder = require("hud/rupees")
-  local small_keys_builder = require("hud/small_keys")
-  local floor_builder = require("hud/floor")
-  local attack_icon_builder = require("hud/attack_icon")
-  local pause_icon_builder = require("hud/pause_icon")
-  local item_icon_builder = require("hud/item_icon")
-  local action_icon_builder = require("hud/action_icon")
+    -- Set up the HUD.
+    local hearts_builder = require("hud/hearts")
+    local magic_bar_builder = require("hud/magic_bar")
+    local rupees_builder = require("hud/rupees")
+    local small_keys_builder = require("hud/small_keys")
+    local floor_builder = require("hud/floor")
+    local attack_icon_builder = require("hud/attack_icon")
+    local pause_icon_builder = require("hud/pause_icon")
+    local item_icon_builder = require("hud/item_icon")
+    local action_icon_builder = require("hud/action_icon")
 
-  self.hud = {  -- Array for the hud elements, table for other hud info.
-    showing_dialog = false,
-    top_left_opacity = 255,
-    custom_command_effects = {},
-  }
+    self.hud = {  -- Array for the hud elements, table for other hud info.
+      showing_dialog = false,
+      top_left_opacity = 255,
+      custom_command_effects = {},
+    }
 
-  local menu = hearts_builder:new(self)
-  menu:set_dst_position(-104, 6)
-  self.hud[#self.hud + 1] = menu
+    local menu = hearts_builder:new(self)
+    menu:set_dst_position(-104, 6)
+    self.hud[#self.hud + 1] = menu
 
-  menu = magic_bar_builder:new(self)
-  menu:set_dst_position(-104, 27)
-  self.hud[#self.hud + 1] = menu
+    menu = magic_bar_builder:new(self)
+    menu:set_dst_position(-104, 27)
+    self.hud[#self.hud + 1] = menu
 
-  menu = rupees_builder:new(self)
-  menu:set_dst_position(8, -20)
-  self.hud[#self.hud + 1] = menu
+    menu = rupees_builder:new(self)
+    menu:set_dst_position(8, -20)
+    self.hud[#self.hud + 1] = menu
 
-  menu = small_keys_builder:new(self)
-  menu:set_dst_position(-36, -18)
-  self.hud[#self.hud + 1] = menu
+    menu = small_keys_builder:new(self)
+    menu:set_dst_position(-36, -18)
+    self.hud[#self.hud + 1] = menu
 
-  menu = floor_builder:new(self)
-  menu:set_dst_position(5, 70)
-  self.hud[#self.hud + 1] = menu
+    menu = floor_builder:new(self)
+    menu:set_dst_position(5, 70)
+    self.hud[#self.hud + 1] = menu
 
-  menu = pause_icon_builder:new(self)
-  menu:set_dst_position(0, 7)
-  self.hud[#self.hud + 1] = menu
-  self.hud.pause_icon = menu
+    menu = pause_icon_builder:new(self)
+    menu:set_dst_position(0, 7)
+    self.hud[#self.hud + 1] = menu
+    self.hud.pause_icon = menu
 
-  menu = item_icon_builder:new(self, 1)
-  menu:set_dst_position(11, 29)
-  self.hud[#self.hud + 1] = menu
-  self.hud.item_icon_1 = menu
+    menu = item_icon_builder:new(self, 1)
+    menu:set_dst_position(11, 29)
+    self.hud[#self.hud + 1] = menu
+    self.hud.item_icon_1 = menu
 
-  menu = item_icon_builder:new(self, 2)
-  menu:set_dst_position(63, 29)
-  self.hud[#self.hud + 1] = menu
-  self.hud.item_icon_2 = menu
+    menu = item_icon_builder:new(self, 2)
+    menu:set_dst_position(63, 29)
+    self.hud[#self.hud + 1] = menu
+    self.hud.item_icon_2 = menu
 
-  menu = attack_icon_builder:new(self)
-  menu:set_dst_position(13, 29)
-  self.hud[#self.hud + 1] = menu
-  self.hud.attack_icon = menu
+    menu = attack_icon_builder:new(self)
+    menu:set_dst_position(13, 29)
+    self.hud[#self.hud + 1] = menu
+    self.hud.attack_icon = menu
 
-  menu = action_icon_builder:new(self)
-  menu:set_dst_position(26, 51)
-  self.hud[#self.hud + 1] = menu
-  self.hud.action_icon = menu
+    menu = action_icon_builder:new(self)
+    menu:set_dst_position(26, 51)
+    self.hud[#self.hud + 1] = menu
+    self.hud.action_icon = menu
 
-  self:set_hud_enabled(true)
+    self:set_hud_enabled(true)
 
-  self:check_hud()
-end
-
-function game:quit_hud()
-
-  if self:is_hud_enabled() then
-    -- Stop all HUD menus.
-    self:set_hud_enabled(false)
-  end
-  self.hud = nil
-end
-
-function game:check_hud()
-
-  local map = self:get_map()
-  if map ~= nil then
-    -- If the hero is below the top-left icons, make them semi-transparent.
-    local hero = map:get_entity("hero")
-    local hero_x, hero_y = hero:get_position()
-    local camera_x, camera_y = map:get_camera():get_bounding_box()
-    local x = hero_x - camera_x
-    local y = hero_y - camera_y
-    local opacity = nil
-
-    if self.hud.top_left_opacity == 255
-        and not self:is_suspended()
-        and x < 88
-        and y < 80 then
-      opacity = 96
-    elseif self.hud.top_left_opacity == 96
-        and (self:is_suspended()
-        or x >= 88
-        or y >= 80) then
-      opacity = 255
-    end
-
-    if opacity ~= nil then
-      self.hud.top_left_opacity = opacity
-      self.hud.item_icon_1.surface:set_opacity(opacity)
-      self.hud.item_icon_2.surface:set_opacity(opacity)
-      self.hud.pause_icon.surface:set_opacity(opacity)
-      self.hud.attack_icon.surface:set_opacity(opacity)
-      self.hud.action_icon.surface:set_opacity(opacity)
-    end
-
-    -- During a dialog, move the action icon and the sword icon.
-    if not self.hud.showing_dialog and
-        game:is_dialog_enabled() then
-      self.hud.showing_dialog = true
-      self.hud.action_icon:set_dst_position(0, 54)
-      self.hud.attack_icon:set_dst_position(0, 29)
-    elseif self.hud.showing_dialog and
-        not game:is_dialog_enabled() then
-      self.hud.showing_dialog = false
-      self.hud.action_icon:set_dst_position(26, 51)
-      self.hud.attack_icon:set_dst_position(13, 29)
-    end
-  end
-
-  sol.timer.start(self, 50, function()
     self:check_hud()
-  end)
-end
+  end
 
-function game:hud_on_map_changed(map)
+  function game:quit_hud()
 
-  if self:is_hud_enabled() then
-    for _, menu in ipairs(self.hud) do
-      if menu.on_map_changed ~= nil then
-        menu:on_map_changed(map)
+    if self:is_hud_enabled() then
+      -- Stop all HUD menus.
+      self:set_hud_enabled(false)
+    end
+    self.hud = nil
+  end
+
+  function game:check_hud()
+
+    local map = self:get_map()
+    if map ~= nil then
+      -- If the hero is below the top-left icons, make them semi-transparent.
+      local hero = map:get_entity("hero")
+      local hero_x, hero_y = hero:get_position()
+      local camera_x, camera_y = map:get_camera():get_bounding_box()
+      local x = hero_x - camera_x
+      local y = hero_y - camera_y
+      local opacity = nil
+
+      if self.hud.top_left_opacity == 255
+          and not self:is_suspended()
+          and x < 88
+          and y < 80 then
+        opacity = 96
+      elseif self.hud.top_left_opacity == 96
+          and (self:is_suspended()
+          or x >= 88
+          or y >= 80) then
+        opacity = 255
+      end
+
+      if opacity ~= nil then
+        self.hud.top_left_opacity = opacity
+        self.hud.item_icon_1.surface:set_opacity(opacity)
+        self.hud.item_icon_2.surface:set_opacity(opacity)
+        self.hud.pause_icon.surface:set_opacity(opacity)
+        self.hud.attack_icon.surface:set_opacity(opacity)
+        self.hud.action_icon.surface:set_opacity(opacity)
+      end
+
+      -- During a dialog, move the action icon and the sword icon.
+      if not self.hud.showing_dialog and
+          game:is_dialog_enabled() then
+        self.hud.showing_dialog = true
+        self.hud.action_icon:set_dst_position(0, 54)
+        self.hud.attack_icon:set_dst_position(0, 29)
+      elseif self.hud.showing_dialog and
+          not game:is_dialog_enabled() then
+        self.hud.showing_dialog = false
+        self.hud.action_icon:set_dst_position(26, 51)
+        self.hud.attack_icon:set_dst_position(13, 29)
+      end
+    end
+
+    sol.timer.start(self, 50, function()
+      self:check_hud()
+    end)
+  end
+
+  function game:hud_on_map_changed(map)
+
+    if self:is_hud_enabled() then
+      for _, menu in ipairs(self.hud) do
+        if menu.on_map_changed ~= nil then
+          menu:on_map_changed(map)
+        end
       end
     end
   end
-end
 
-function game:hud_on_paused()
+  function game:hud_on_paused()
 
-  if self:is_hud_enabled() then
-    for _, menu in ipairs(self.hud) do
-      if menu.on_paused ~= nil then
-        menu:on_paused()
+    if self:is_hud_enabled() then
+      for _, menu in ipairs(self.hud) do
+        if menu.on_paused ~= nil then
+          menu:on_paused()
+        end
       end
     end
   end
-end
 
-function game:hud_on_unpaused()
+  function game:hud_on_unpaused()
 
-  if self:is_hud_enabled() then
-    for _, menu in ipairs(self.hud) do
-      if menu.on_unpaused ~= nil then
-        menu:on_unpaused()
+    if self:is_hud_enabled() then
+      for _, menu in ipairs(self.hud) do
+        if menu.on_unpaused ~= nil then
+          menu:on_unpaused()
+        end
       end
     end
   end
-end
 
-function game:is_hud_enabled()
-  return self.hud_enabled
-end
+  function game:is_hud_enabled()
+    return self.hud_enabled
+  end
 
-function game:set_hud_enabled(hud_enabled)
+  function game:set_hud_enabled(hud_enabled)
 
-  if hud_enabled ~= self.hud_enabled then
-    game.hud_enabled = hud_enabled
+    if hud_enabled ~= self.hud_enabled then
+      game.hud_enabled = hud_enabled
 
-    for _, menu in ipairs(self.hud) do
-      if hud_enabled then
-        sol.menu.start(self, menu)
-      else
-        sol.menu.stop(menu)
+      for _, menu in ipairs(self.hud) do
+        if hud_enabled then
+          sol.menu.start(self, menu)
+        else
+          sol.menu.stop(menu)
+        end
       end
     end
   end
-end
 
-function game:get_custom_command_effect(command)
+  function game:get_custom_command_effect(command)
 
-  return self.hud.custom_command_effects[command]
-end
-
--- Make the action (or attack) icon of the HUD show something else than the
--- built-in effect or the action (or attack) command.
--- You are responsible to override the command if you don't want the built-in
--- effect to be performed.
--- Set the effect to nil to show the built-in effect again.
-function game:set_custom_command_effect(command, effect)
-
-  if self.hud ~= nil then
-    self.hud.custom_command_effects[command] = effect
+    return self.hud.custom_command_effects[command]
   end
-end
 
+  -- Make the action (or attack) icon of the HUD show something else than the
+  -- built-in effect or the action (or attack) command.
+  -- You are responsible to override the command if you don't want the built-in
+  -- effect to be performed.
+  -- Set the effect to nil to show the built-in effect again.
+  function game:set_custom_command_effect(command, effect)
+
+    if self.hud ~= nil then
+      self.hud.custom_command_effects[command] = effect
+    end
+  end
+
+end
--- data/menus/game_over.lua
+++ data/menus/game_over.lua
@@ -1,194 +1,195 @@
-local game = ...
+return function(game)
 
-local game_over_menu = {}  -- The game-over menu.
+  local game_over_menu = {}  -- The game-over menu.
 
-local music
-local background_img
-local hero_was_visible
-local hero_dead_sprite
-local hero_dead_x, hero_dead_y
-local fade_sprite
-local fairy_sprite
-local cursor_position
-local state
+  local music
+  local background_img
+  local hero_was_visible
+  local hero_dead_sprite
+  local hero_dead_x, hero_dead_y
+  local fade_sprite
+  local fairy_sprite
+  local cursor_position
+  local state
 
--- state can be one of:
--- "waiting_start": The game-over scene will start soon.
--- "closing_game": Fade-out on the game screen.
--- "red_screen": Red screen during a small delay.
--- "opening_menu": Fade-in on the game-over menu.
--- "saved_by_fairy": The player is being saved by a fairy.
--- "waiting_end": The game will be resumed soon.
--- "resume_game": The game can be resumed.
--- "menu": The player can choose an option in the game-over menu.
--- "finished": An action was validated in the menu.
+  -- state can be one of:
+  -- "waiting_start": The game-over scene will start soon.
+  -- "closing_game": Fade-out on the game screen.
+  -- "red_screen": Red screen during a small delay.
+  -- "opening_menu": Fade-in on the game-over menu.
+  -- "saved_by_fairy": The player is being saved by a fairy.
+  -- "waiting_end": The game will be resumed soon.
+  -- "resume_game": The game can be resumed.
+  -- "menu": The player can choose an option in the game-over menu.
+  -- "finished": An action was validated in the menu.
 
-function game:on_game_over_started()
+  function game:on_game_over_started()
 
-  -- Attach the game-over menu to the map so that the map's fade-out
-  -- effect applies to it when restarting the game.
-  sol.menu.start(game:get_map(), game_over_menu)
-end
+    -- Attach the game-over menu to the map so that the map's fade-out
+    -- effect applies to it when restarting the game.
+    sol.menu.start(game:get_map(), game_over_menu)
+  end
 
-function game_over_menu:on_started()
+  function game_over_menu:on_started()
 
-  local hero = game:get_hero()
-  hero_was_visible = hero:is_visible()
-  hero:set_visible(false)
-  music = sol.audio.get_music()
-  background_img = sol.surface.create("gameover_menu.png", true)
-  local tunic = game:get_ability("tunic")
-  hero_dead_sprite = sol.sprite.create("hero/tunic" .. tunic)
-  hero_dead_sprite:set_animation("hurt")
-  hero_dead_sprite:set_direction(hero:get_direction())
-  hero_dead_sprite:set_paused(true)
-  fade_sprite = sol.sprite.create("hud/gameover_fade")
-  fairy_sprite = sol.sprite.create("entities/items")
-  fairy_sprite:set_animation("fairy")
-  state = "waiting_start"
+    local hero = game:get_hero()
+    hero_was_visible = hero:is_visible()
+    hero:set_visible(false)
+    music = sol.audio.get_music()
+    background_img = sol.surface.create("gameover_menu.png", true)
+    local tunic = game:get_ability("tunic")
+    hero_dead_sprite = sol.sprite.create("hero/tunic" .. tunic)
+    hero_dead_sprite:set_animation("hurt")
+    hero_dead_sprite:set_direction(hero:get_direction())
+    hero_dead_sprite:set_paused(true)
+    fade_sprite = sol.sprite.create("hud/gameover_fade")
+    fairy_sprite = sol.sprite.create("entities/items")
+    fairy_sprite:set_animation("fairy")
+    state = "waiting_start"
 
-  local map = game:get_map()
-  local camera_x, camera_y = map:get_camera():get_bounding_box()
-  local hero_x, hero_y = hero:get_position()
-  hero_dead_x = hero_x - camera_x
-  hero_dead_y = hero_y - camera_y
+    local map = game:get_map()
+    local camera_x, camera_y = map:get_camera():get_bounding_box()
+    local hero_x, hero_y = hero:get_position()
+    hero_dead_x = hero_x - camera_x
+    hero_dead_y = hero_y - camera_y
 
-  sol.timer.start(self, 500, function()
-    state = "closing_game"
-    sol.audio.stop_music()
-    fade_sprite:set_animation("close")
-    fade_sprite.on_animation_finished = function()
-      if state == "closing_game" then
-        state = "red_screen"
-        sol.audio.play_sound("hero_dying")
-        hero_dead_sprite:set_paused(false)
-        hero_dead_sprite:set_direction(0)
-        hero_dead_sprite:set_animation("dying")
-        sol.timer.start(self, 2000, function()
-          state = "opening_menu"
-          fade_sprite:set_animation("open")
-        end)
-      elseif state == "opening_menu" then
-        local bottle_with_fairy = game:get_first_bottle_with(6)
-        if bottle_with_fairy ~= nil then
-          -- Has a fairy.
-          state = "saved_by_fairy"
-
-          -- Make the bottle empty.
-          bottle_with_fairy:set_variant(1)
-          fairy_sprite:set_xy(hero_dead_x + 12, hero_dead_y + 21)
-          local movement = sol.movement.create("target")
-          movement:set_target(240, 22)
-          movement:set_speed(96)
-
-          movement:start(fairy_sprite, function()
-            state = "waiting_end"
-            game:add_life(7 * 4)  -- Restore 7 hearts.
-            sol.timer.start(self, 1000, function()
-              state = "resume_game"
-              sol.audio.play_music(music)
-              game:stop_game_over()
-              sol.menu.stop(self)
-            end)
+    sol.timer.start(self, 500, function()
+      state = "closing_game"
+      sol.audio.stop_music()
+      fade_sprite:set_animation("close")
+      fade_sprite.on_animation_finished = function()
+        if state == "closing_game" then
+          state = "red_screen"
+          sol.audio.play_sound("hero_dying")
+          hero_dead_sprite:set_paused(false)
+          hero_dead_sprite:set_direction(0)
+          hero_dead_sprite:set_animation("dying")
+          sol.timer.start(self, 2000, function()
+            state = "opening_menu"
+            fade_sprite:set_animation("open")
           end)
-        else
-          -- No fairy: game over.
-          state = "menu"
-          sol.audio.play_music("game_over")
-          fairy_sprite:set_xy(76, 112)  -- Cursor.
-          cursor_position = 0
-        end
+        elseif state == "opening_menu" then
+          local bottle_with_fairy = game:get_first_bottle_with(6)
+          if bottle_with_fairy ~= nil then
+            -- Has a fairy.
+            state = "saved_by_fairy"
 
+            -- Make the bottle empty.
+            bottle_with_fairy:set_variant(1)
+            fairy_sprite:set_xy(hero_dead_x + 12, hero_dead_y + 21)
+            local movement = sol.movement.create("target")
+            movement:set_target(240, 22)
+            movement:set_speed(96)
+
+            movement:start(fairy_sprite, function()
+              state = "waiting_end"
+              game:add_life(7 * 4)  -- Restore 7 hearts.
+              sol.timer.start(self, 1000, function()
+                state = "resume_game"
+                sol.audio.play_music(music)
+                game:stop_game_over()
+                sol.menu.stop(self)
+              end)
+            end)
+          else
+            -- No fairy: game over.
+            state = "menu"
+            sol.audio.play_music("game_over")
+            fairy_sprite:set_xy(76, 112)  -- Cursor.
+            cursor_position = 0
+          end
+
+        end
+      end
+    end)
+  end
+
+  function game_over_menu:on_finished()
+
+    local hero = game:get_hero()
+    if hero ~= nil then
+      hero:set_visible(hero_was_visible)
+    end
+    music = nil
+    background_img = nil
+    hero_dead_sprite = nil
+    fade_sprite = nil
+    fairy_sprite = nil
+    cursor_position = nil
+    state = nil
+    sol.timer.stop_all(self)
+  end
+
+  local black = {0, 0, 0}
+  local red = {224, 32, 32}
+
+  function game_over_menu:on_draw(dst_surface)
+
+    if state ~= "waiting_start" and state ~= "closing_game" then
+      -- Hide the whole map.
+      dst_surface:fill_color(black)
+    end
+
+    if state == "closing_game"
+        or state == "opening_menu" then
+      fade_sprite:draw(dst_surface, hero_dead_x, hero_dead_y)
+    elseif state == "red_screen" then
+      dst_surface:fill_color(red)
+    end
+
+    if state == "menu" or state == "finished" then
+      background_img:draw(dst_surface)
+      fairy_sprite:draw(dst_surface)
+    elseif state ~= "resume_game" then
+      hero_dead_sprite:draw(dst_surface, hero_dead_x, hero_dead_y)
+      if state == "saved_by_fairy" then
+        fairy_sprite:draw(dst_surface)
       end
     end
-  end)
-end
-
-function game_over_menu:on_finished()
-
-  local hero = game:get_hero()
-  if hero ~= nil then
-    hero:set_visible(hero_was_visible)
-  end
-  music = nil
-  background_img = nil
-  hero_dead_sprite = nil
-  fade_sprite = nil
-  fairy_sprite = nil
-  cursor_position = nil
-  state = nil
-  sol.timer.stop_all(self)
-end
-
-local black = {0, 0, 0}
-local red = {224, 32, 32}
-
-function game_over_menu:on_draw(dst_surface)
-
-  if state ~= "waiting_start" and state ~= "closing_game" then
-    -- Hide the whole map.
-    dst_surface:fill_color(black)
   end
 
-  if state == "closing_game"
-      or state == "opening_menu" then
-    fade_sprite:draw(dst_surface, hero_dead_x, hero_dead_y)
-  elseif state == "red_screen" then
-    dst_surface:fill_color(red)
-  end
+  function game_over_menu:on_command_pressed(command)
 
-  if state == "menu" or state == "finished" then
-    background_img:draw(dst_surface)
-    fairy_sprite:draw(dst_surface)
-  elseif state ~= "resume_game" then
-    hero_dead_sprite:draw(dst_surface, hero_dead_x, hero_dead_y)
-    if state == "saved_by_fairy" then
-      fairy_sprite:draw(dst_surface)
+    if state ~= "menu" then
+      -- Commands are not available during the game-over opening animations.
+      return
+    end
+
+    if command == "down" then
+      sol.audio.play_sound("cursor")
+      cursor_position = (cursor_position + 1) % 4
+      local fairy_x, fairy_y = fairy_sprite:get_xy()
+      fairy_y = 112 + cursor_position * 16
+      fairy_sprite:set_xy(fairy_x, fairy_y)
+    elseif command == "up" then
+      sol.audio.play_sound("cursor")
+      cursor_position = (cursor_position + 3) % 4
+      local fairy_x, fairy_y = fairy_sprite:get_xy()
+      fairy_y = 112 + cursor_position * 16
+      fairy_sprite:set_xy(fairy_x, fairy_y)
+    elseif command == "action" or command == "attack" then
+
+      state = "finished"
+      sol.audio.play_sound("danger")
+      game:set_hud_enabled(false)
+      game:add_life(7 * 4)  -- Restore 7 hearts.
+
+      if cursor_position == 0 then
+        -- Save and continue.
+        game:save()
+        game:start()
+      elseif cursor_position == 1 then
+        -- Save and quit.
+        game:save()
+        sol.main.reset()
+      elseif cursor_position == 2 then
+        -- Continue without saving.
+        game:start()
+      elseif cursor_position == 3 then
+        -- Quit without saving.
+        sol.main.reset()
+      end
     end
   end
+
 end
-
-function game_over_menu:on_command_pressed(command)
-
-  if state ~= "menu" then
-    -- Commands are not available during the game-over opening animations.
-    return
-  end
-
-  if command == "down" then
-    sol.audio.play_sound("cursor")
-    cursor_position = (cursor_position + 1) % 4
-    local fairy_x, fairy_y = fairy_sprite:get_xy()
-    fairy_y = 112 + cursor_position * 16
-    fairy_sprite:set_xy(fairy_x, fairy_y)
-  elseif command == "up" then
-    sol.audio.play_sound("cursor")
-    cursor_position = (cursor_position + 3) % 4
-    local fairy_x, fairy_y = fairy_sprite:get_xy()
-    fairy_y = 112 + cursor_position * 16
-    fairy_sprite:set_xy(fairy_x, fairy_y)
-  elseif command == "action" or command == "attack" then
-
-    state = "finished"
-    sol.audio.play_sound("danger")
-    game:set_hud_enabled(false)
-    game:add_life(7 * 4)  -- Restore 7 hearts.
-
-    if cursor_position == 0 then
-      -- Save and continue.
-      game:save()
-      game:start()
-    elseif cursor_position == 1 then
-      -- Save and quit.
-      game:save()
-      sol.main.reset()
-    elseif cursor_position == 2 then
-      -- Continue without saving.
-      game:start()
-    elseif cursor_position == 3 then
-      -- Quit without saving.
-      sol.main.reset()
-    end
-  end
-end
-
